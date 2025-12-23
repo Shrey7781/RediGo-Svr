1ce44c78c72165338c2407f93e5f3e41ba6d0bc6
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2025-12-23 14:49:44 +0530

    implemented Redis Serialisation Protocol RESP

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Shrey7781/redis-clone
+module github.com/Shrey7781/RediGo-Svr
 
 go 1.25.5
--- main.go
+++ main.go
@@ -5,7 +5,55 @@ import (
 	"log"
 	"net"
 	"os"
+	"io"
+	"strconv"
 )
+
+type ValueType string
+
+const(
+	ARRAY ValueType = "*"
+	BULK ValueType = "$"
+	STRING ValueType = "+"
+)
+
+type Value struct{
+	typ ValueType
+	bulk string
+	str string
+	array []Value
+}
+
+func (v *Value) readArray (reader io.Reader){
+	buf:= make([]byte,4)
+	reader.Read(buf)
+	arrLen, err:=strconv.Atoi(string(buf[1]))
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	for range arrLen{
+		bulk := v.readBulk(reader)
+		v.array= append(v.array,bulk)
+	}
+}
+
+func(v *Value) readBulk(reader io.Reader) Value{
+	buf:= make([]byte,4)
+	reader.Read(buf)
+
+	n,err:=strconv.Atoi(string(buf[1]))
+	if err!=nil{
+		fmt.Println(err)
+		return Value{}
+	}
+	bulkBuf:= make([]byte, n+2)
+	reader.Read(bulkBuf)
+
+	bulk:= string(bulkBuf[:n])
+	return Value{typ: BULK, bulk: bulk}
+}
 
 func main(){
 	ln,err := net.Listen("tcp", "localhost:6379")
@@ -21,15 +69,11 @@ func main(){
 	 }
 	 defer conn.Close()
 
-	 buf:= make([]byte, 1024)
-	 conn.Read(buf)
-
-	fmt.Println(string(buf))
-
+	for{
+	v:= Value{typ: ARRAY}
+	v.readArray(conn)
+	fmt.Println(v.array)
 	conn.Write([]byte("+OK\r\n"))
-
-	
-
-
+	 }
 
 }
